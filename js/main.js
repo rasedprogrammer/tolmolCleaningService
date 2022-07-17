c0fed3ae9f0b5a38042b1bf22bece45a7643b6d7
unknown <rasedprogrammer@gmail.com>
2022-07-17 22:12:17 +0600

    test

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const headerBg = () => {
+    const header = document.querySelector(".js-header");
+    window.addEventListener("scroll", function(){
+        if(this.scrollY > 0){
+            header.classList.add("bg-reveal");
+        }else{
+            header.classList.remove("bg-reveal");
+        }
+    })
+}
+headerBg();
