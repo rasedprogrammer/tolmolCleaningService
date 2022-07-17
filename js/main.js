3e67516a9e8fb7eb9a905e9d4829c5c2a0ac578c
unknown <rasedprogrammer@gmail.com>
2022-07-17 22:28:12 +0600

    uploading header menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const headerBg = () => {
     const header = document.querySelector(".js-header");
-    window.addEventListener("scroll", function(){
-        if(this.scrollY > 0){
+    window.addEventListener("scroll", function () {
+        if (this.scrollY > 0) {
             header.classList.add("bg-reveal");
-        }else{
+        } else {
             header.classList.remove("bg-reveal");
         }
     })
